f46ab63e159aa26419cc367548637ce033e0334f
breakthestatic <breakthestatic@gmail.com>
2024-12-11 18:32:39 -0800

    Correct data errors with DateEnd (#1)
    * Correct end date data errors
    
    * move date correction inside first call, tighten condition to exactly one day
    
    ---------
    
    Co-authored-by: MattDahEpic <MattDahEpic@users.noreply.github.com>

--- california_midasapi/types.py
+++ california_midasapi/types.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass, field
 from typing import Optional
-from datetime import datetime
+from datetime import datetime, timedelta
 from dateutil import parser
 
 @dataclass
@@ -34,6 +34,13 @@ class ValueInfoItem:
         """Get the end of this tariff as a python datetime"""
         if self.__endDateTime is None:
             self.__endDateTime = parser.parse(f"{self.DateEnd} {self.TimeEnd}Z")
+
+            """Correct end dates that incorrectly cross over into next day (#1)"""
+            startDate = parser.parse(self.DateStart)
+            endDate = parser.parse(self.DateEnd)
+            if endDate - startDate == timedelta(days=1) and self.DayStart == self.DayEnd:
+                self.__endDateTime = parser.parse(f"{self.DateStart} {self.TimeEnd}Z")
+
         return self.__endDateTime
 
 @dataclass
