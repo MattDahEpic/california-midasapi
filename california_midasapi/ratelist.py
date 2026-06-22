917b0bcf8fb11f12fc05ce768ada81f5b0c4c736
m <matt@mattdahepic.com>
2026-06-22 17:22:16 +0000

    centralize current and historical parsing

--- california_midasapi/ratelist.py
+++ california_midasapi/ratelist.py
@@ -31,27 +31,26 @@ class Midas(MidasInternal):
         """
         Returns data about a given rate.
         """
-
-        def __rateInfoObjectHook(dict: dict[Any, Any]):
-            # this has to handle both the parent object and its children
-            if "DateStart" in dict:
-                return ValueInfoItem(**dict)
-            elif "RateID" in dict:
-                return RateInfo(**dict)
-            else:
-                raise MidasDecodingException("Invalid object type for __rateInfoObjectHook")
             
         # TODO what does queryType=realtime even do? all properties are None
         url = 'https://midasapi.energy.ca.gov/api/valuedata?id=' + rateID + '&querytype=' + queryType
         pricing_response = await self._request('GET', url)
 
-        return (json.loads(pricing_response, object_hook=__rateInfoObjectHook))
+        return self.__parseRateInfoResponse(pricing_response)
     
     async def GetHistoricalRateInfo(self, rateID: str, startDate: datetime, endDate: datetime) -> RateInfo:
         """
         Returns historical data about a given rate during the specified time period.
-        """
+        """            
+        url = 'https://midasapi.energy.ca.gov/api/historicaldata/' + rateID + '?startdate=' + startDate.strftime("%Y-%m-%d") + '&enddate=' + endDate.strftime("%Y-%m-%d")
+        pricing_response = await self._request('GET', url)
 
+        return self.__parseRateInfoResponse(pricing_response)
+    
+
+    @staticmethod
+    def __parseRateInfoResponse(response: str) -> RateInfo:
+        """Parse a RateInfo from string."""
         def __rateInfoObjectHook(dict: dict[Any, Any]):
             # this has to handle both the parent object and its children
             if "DateStart" in dict:
@@ -60,8 +59,5 @@ class Midas(MidasInternal):
                 return RateInfo(**dict)
             else:
                 raise MidasDecodingException("Invalid object type for __rateInfoObjectHook")
-            
-        url = 'https://midasapi.energy.ca.gov/api/historicaldata/' + rateID + '?startdate=' + startDate.strftime("%Y-%m-%d") + '&enddate=' + endDate.strftime("%Y-%m-%d")
-        pricing_response = await self._request('GET', url)
-
-        return (json.loads(pricing_response, object_hook=__rateInfoObjectHook))
+        
+        return json.loads(response, object_hook=__rateInfoObjectHook)
